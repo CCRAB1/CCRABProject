c492b33e0f191502395a5720388c64e6d7186b53
DANRAMAGE <ChiefDan@gmail.com>
2026-07-14 13:41:20 -0400

    Add active into the PlatformInfo class.

--- apps/CCRABDashBoard/platforms_app/static/platforms_app/js/platform_info.js
+++ apps/CCRABDashBoard/platforms_app/static/platforms_app/js/platform_info.js
@@ -148,7 +148,7 @@ export class PlatformInfo {
     return {
       shortName: source.short_name || "",
       order: order,
-      active: source.active || 1,
+      active: source.active,
       channelLabel: order === 1 ? "A" : "B",
       obsStandardName: source.obs_standard_name || "",
       obsDefinition: source.obs_definition || "",
