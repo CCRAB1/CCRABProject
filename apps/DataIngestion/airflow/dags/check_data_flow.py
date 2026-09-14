ff9146d1f7589d06c17797e0db98459eeb9c921a
DANRAMAGE <ChiefDan@gmail.com>
2026-09-14 09:30:54 -0400

    Implmented the data freshness tables.

--- apps/DataIngestion/airflow/dags/check_data_flow.py
+++ apps/DataIngestion/airflow/dags/check_data_flow.py
@@ -85,6 +85,8 @@ def check_data_flow():
             .order_by("platform_handle")
         )
         df = pd.DataFrame.from_records(platforms)
+        for index, row in df.iterrows():
+            logger.info(f"{df['platform_handle']} last reported: {row['latest_m_date']}")
         output_file = base_directory / f"check_data_flow-{now_time.timestamp()}.csv"
         df.to_csv(output_file, index=False)
 
